96bb0eee74f32fde2469a47be8edddc0db32fdd4
Aziz Muslim <aziez.musleam@gmail.com>
2023-08-31 22:47:33 +0700

    tambah komentar

--- Task_Jubelio/Login.py
+++ Task_Jubelio/Login.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+# inport jeda waktu
 import time
 
 driver = webdriver.Chrome()
