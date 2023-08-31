bf7323300a978b7006be0662ab9b356f3e94ac57
AziezM <aziez.musleam@gmail.com>
2023-08-31 22:49:58 +0700

    add comment url

--- Task_Jubelio/Login.py
+++ Task_Jubelio/Login.py
@@ -6,7 +6,7 @@ import time
 
 driver = webdriver.Chrome()
 driver.maximize_window()
-
+# menambahkan alamat url
 driver.get("https://app.jubelio.com/login")
 elem = driver.find_element(By.XPATH,"//input[@name='email']").send_keys("qa.rakamin.jubelio@gmail.com")
 elem = driver.find_element(By.XPATH,"//input[@name='password']").send_keys("Jubelio123!")
